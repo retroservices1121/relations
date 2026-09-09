4f8b34fca6d405bff0593ef6ba0b718fa2ec666f
retroservices1121 <76626942+retroservices1121@users.noreply.github.com>
2026-09-09 00:29:57 -0400

    Load extra Relations Studio episodes

--- app/episodes/[id]/page.tsx
+++ app/episodes/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from "next/navigation";
 import EpisodeWorkspace from "../../../components/EpisodeWorkspace";
-import { episodes } from "../../../data/episodes";
+import { episodes as baseEpisodes } from "../../../data/episodes";
+import { extraEpisodes } from "../../../data/extraEpisodes";
+
+const episodes = [...baseEpisodes, ...extraEpisodes];
 
 export default async function EpisodePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
