6538e7a7d2f87e56104aac937e2dce0c1514429e
retroservices1121 <76626942+retroservices1121@users.noreply.github.com>
2026-09-12 09:39:46 -0400

    Wire musical episode into studio

--- app/episodes/[id]/page.tsx
+++ app/episodes/[id]/page.tsx
@@ -1,17 +1,21 @@
 import { notFound } from "next/navigation";
 import EpisodeWorkspace from "../../../components/EpisodeWorkspace";
 import FalCostDashboard from "../../../components/FalCostDashboard";
+import MusicalEpisodePanel from "../../../components/MusicalEpisodePanel";
 import { episodes as baseEpisodes } from "../../../data/episodes";
 import { extraEpisodes } from "../../../data/extraEpisodes";
+import { musicalEpisodes } from "../../../data/musicalEpisodes";
 
-const episodes = [...baseEpisodes, ...extraEpisodes];
+const episodes = [...baseEpisodes, ...extraEpisodes, ...musicalEpisodes];
 
 export default async function EpisodePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const episode = episodes.find((item) => item.id === id);
   if (!episode) notFound();
+  const musical = musicalEpisodes.find((item) => item.id === id);
   return <>
     <EpisodeWorkspace episode={episode} />
+    {musical && <MusicalEpisodePanel episode={musical} />}
     <FalCostDashboard episodeId={episode.id} />
   </>;
 }
